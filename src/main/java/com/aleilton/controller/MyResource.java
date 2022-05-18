52ee7f765988083d5ed0397f1044e13923200f70
Aleilton Chavenco <aleilton@gmail.com>
2022-05-18 11:23:02 -0300

    feat: implement routes controller

--- src/main/java/com/aleilton/controller/MyResource.java
+++ src/main/java/com/aleilton/controller/MyResource.java
@@ -11,36 +11,34 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import com.aleilton.controller.model.Pessoa;
+import com.aleilton.controller.model.PessoaDAO;
 import com.google.gson.Gson;
 
 @Path("api")
 public class MyResource {
-//  private DAO dao = new DAO();
+  private PessoaDAO dao = new PessoaDAO();
 
   @GET
   @Produces("application/json")
   public Response getPessoas() {
-//    ArrayList<Pessoa> pessoas = dao.findAll();
-//    return Response.ok(new Gson().toJson(pessoas)).build();
-    return Response.ok("Deu certo").build();
+    ArrayList<Pessoa> pessoas = dao.findAll();
+    return Response.ok(new Gson().toJson(pessoas)).build();
   }
 
   @POST
   @Consumes("application/json")
   @Produces("application/json")
-  public Response postPessoas() {
-//    public Response postPessoas(Pessoa pessoa) {
-//    String result = dao.insert(pessoa);
-//    return Response.ok(result).status(201).build();
-    return Response.ok("POST").status(201).build();
+  public Response postPessoas(Pessoa pessoa) {
+    String result = dao.insert(pessoa);
+    return Response.ok(result).status(201).build();
   }
   
   @Path("/{id}")
   @DELETE
   @Consumes("application/json")
   public Response deletePessoa(@PathParam("id") String id) {
-//    dao.delete(id);
-//    return Response.ok().status(204).build();
+    dao.delete(id);
     return Response.ok().status(204).build();
   }
 
